db67c534f2528ca1c984195815a6b07906852267
npag <npag@users.noreply.github.com>
2015-09-13 22:17:43 +0200

    initial commit

---
+++ WhoopWhoop3000/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Soundboard
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.updateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.tmrHide = new System.Windows.Forms.Timer(this.components);
+            this.lblText = new System.Windows.Forms.Label();
+            this.lblFolder = new System.Windows.Forms.Label();
+            this.contextMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenu;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Soundboard";
+            this.notifyIcon.Visible = true;
+            // 
+            // contextMenu
+            // 
+            this.contextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.updateToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.quitToolStripMenuItem});
+            this.contextMenu.Name = "contextMenu";
+            this.contextMenu.Size = new System.Drawing.Size(113, 54);
+            // 
+            // updateToolStripMenuItem
+            // 
+            this.updateToolStripMenuItem.Name = "updateToolStripMenuItem";
+            this.updateToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.updateToolStripMenuItem.Text = "&Update";
+            this.updateToolStripMenuItem.Click += new System.EventHandler(this.updateToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(109, 6);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.quitToolStripMenuItem.Text = "&Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Image = global::WhoopWhoop3000.Properties.Resources.Sound_icon;
+            this.pictureBox.Location = new System.Drawing.Point(6, 6);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(51, 50);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // tmrHide
+            // 
+            this.tmrHide.Interval = 1000;
+            this.tmrHide.Tick += new System.EventHandler(this.tmrHide_Tick);
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Font = new System.Drawing.Font("Ubuntu", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblText.Location = new System.Drawing.Point(63, 36);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(87, 17);
+            this.lblText.TabIndex = 1;
+            this.lblText.Text = "Wilkommen";
+            // 
+            // lblFolder
+            // 
+            this.lblFolder.AutoSize = true;
+            this.lblFolder.Font = new System.Drawing.Font("Ubuntu", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFolder.ForeColor = System.Drawing.Color.Black;
+            this.lblFolder.Location = new System.Drawing.Point(63, 20);
+            this.lblFolder.Name = "lblFolder";
+            this.lblFolder.Size = new System.Drawing.Size(14, 16);
+            this.lblFolder.TabIndex = 3;
+            this.lblFolder.Text = "-";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(213)))), ((int)(((byte)(166)))));
+            this.ClientSize = new System.Drawing.Size(300, 62);
+            this.Controls.Add(this.lblFolder);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.pictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "Soundboard";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Timer tmrHide;
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.ContextMenuStrip contextMenu;
+        private System.Windows.Forms.ToolStripMenuItem updateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.Label lblFolder;
+    }
+}
+
---
+++ WhoopWhoop3000/Form1.cs
@@ -0,0 +1,218 @@
+﻿using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Soundboard
+{
+    public partial class frmMain : Form
+    {
+        private IWavePlayer waveOut;
+        private IWavePlayer waveOut2;
+        private string[][] files;
+        private int curFile = 0;
+        private int curDir = 0;
+        private MMDevice device;
+        private MMDevice device2;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Program.NewKey += new Program.KeyHandler(keyEvent);
+            Rectangle workingArea = Screen.GetWorkingArea(this);
+            this.Location = new Point(workingArea.Right - Size.Width, workingArea.Bottom - Size.Height);
+            device = new MMDeviceEnumerator().EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active).FirstOrDefault(d => d.ID == "{0.0.0.00000000}.{a14f7b68-1bb8-4de3-bfd6-38fc5fc43b2d}");
+            device2 = new MMDeviceEnumerator().EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active).FirstOrDefault(d => d.ID == "{0.0.0.00000000}.{4ac56367-03a9-474e-b1e3-c6dfb1f36aad}");//{0.0.0.00000000}.{234779d1-4c87-4bc3-88c9-e03ea41dfbd0}
+
+            update();
+            showFile();
+            tmrHide.Enabled = true;
+        }
+
+        private void keyEvent(string aKey)
+        {
+            if (aKey == "MediaPlayPause")
+            {
+                tmrHide.Enabled = false;
+                this.Show();
+                play();
+                showFile();
+                tmrHide.Enabled = true;
+            }
+
+            if (aKey == "MediaStop")
+            {
+                tmrHide.Enabled = false;
+                this.Show();
+                stop();
+                showFile();
+                tmrHide.Enabled = true;
+            }
+
+            if (aKey == "MediaNextTrack")
+            {
+                tmrHide.Enabled = false;
+                this.Show();
+                next();
+                showFile();
+                tmrHide.Enabled = true;
+            }
+
+            if (aKey == "MediaPreviousTrack")
+            {
+                tmrHide.Enabled = false;
+                this.Show();
+                previous();
+                showFile();
+                tmrHide.Enabled = true;
+            }
+
+            if (aKey == "F7")
+            {
+                tmrHide.Enabled = false;
+                this.Show();
+                previousDir();
+                showFile();
+                tmrHide.Enabled = true;
+            }
+
+            if (aKey == "F8")
+            {
+                tmrHide.Enabled = false;
+                this.Show();
+                nextDir();
+                showFile();
+                tmrHide.Enabled = true;
+            }
+        }
+
+        private void update()
+        {
+            string[] dirs = Directory.GetDirectories(Directory.GetCurrentDirectory() + "\\sounds");
+            files = new string[dirs.Count()][];
+            for (int i = 0; i < dirs.Count(); i++)
+            {
+                files[i] = Directory.GetFiles(dirs[i], "*", SearchOption.AllDirectories);
+            }
+            curFile = 0;
+            curDir = 0;
+        }
+
+        private void play()
+        {
+            try
+            {
+                if (waveOut != null)
+                {
+                    if (waveOut.PlaybackState == PlaybackState.Playing)
+                    {
+                        waveOut.Stop();
+                        waveOut2.Stop();
+                    }
+                }
+                waveOut = new WasapiOut(device, AudioClientShareMode.Shared, false, 0);
+
+                AudioFileReader audioFileReader = new AudioFileReader(files[curDir][curFile]);
+
+                waveOut.Init(audioFileReader);
+                waveOut.Play();
+
+                //MMDevice device2 = new MMDeviceEnumerator().EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active).FirstOrDefault(d => d.ID == "{0.0.0.00000000}.{32867401-43a5-4319-98e3-c8f88b9b8f5f}");
+
+                //	ID	"{0.0.0.00000000}.{234779d1-4c87-4bc3-88c9-e03ea41dfbd0}"	string
+
+                waveOut2 = new WasapiOut(device2, AudioClientShareMode.Shared, false, 0);
+
+                AudioFileReader audioFileReader2 = new AudioFileReader(files[curDir][curFile]);
+                audioFileReader2.Volume = 0.2F;
+                waveOut2.Init(audioFileReader2);
+                waveOut2.Play();
+            }
+            catch
+            {
+                update();
+            }
+        }
+
+        private void next()
+        {
+            if (curFile < files[curDir].Count() - 1) curFile++;
+            else nextDir();
+        }
+
+        private void nextDir()
+        {
+            if (curDir < files.Count() - 1) curDir++;
+            else curDir = 0;
+            curFile = 0;
+        }
+
+        private void stop()
+        {
+            waveOut.Stop();
+            waveOut2.Stop();
+        }
+
+        private void previous()
+        {
+            if (curFile > 0) curFile--;
+            else
+            {
+                previousDir();
+                curFile = files[curDir].Count() - 1;
+            }
+        }
+
+        private void previousDir()
+        {
+            if (curDir > 0) curDir--;
+            else curDir = files.Count() - 1;
+            curFile = 0;
+        }
+
+        private void showFile()
+        {
+            string path = files[curDir][curFile];
+            string[] pathSplit = path.Split('\\');
+            if (pathSplit[pathSplit.Count() - 2] != "sounds") lblFolder.Text = pathSplit[pathSplit.Count() - 2];
+            else lblFolder.Text = "-";
+            string[] fileSplit = pathSplit[pathSplit.Count() - 1].Split('.');
+            lblText.Text = fileSplit[0];
+            for (int counter = 0; counter < 100; counter++)
+            {
+                this.Opacity = 0.01F * counter;
+                Thread.Sleep(1);
+            }
+        }
+
+        private void tmrHide_Tick(object sender, EventArgs e)
+        {
+            for (int counter = 100; counter > 0; counter--)
+            {
+                this.Opacity = 0.01F * counter;
+                Thread.Sleep(1);
+            }
+            this.Hide();
+            tmrHide.Enabled = false;
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            notifyIcon.Visible = false;
+            Environment.Exit(0);
+        }
+
+        private void updateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            update();
+        }
+    }
+}
